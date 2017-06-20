9cc30fc6070fb438e0bb852acc2b943cc257fd9c
JacobMoller <jmj1999@gmail.com>
2017-06-20 13:30:39 +0200

    Added Contact Page
    Added the basic contact page.

---
+++ public_html/contact.php
@@ -0,0 +1,43 @@
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400" rel="stylesheet">
+      <title>Project Team-Up | Contact</title>
+		<style type="text/css">
+		body{
+			margin: 0;
+			padding: 0;
+			font-family: 'Source Sans Pro', sans-serif;
+			font-weight: 300;
+			width: 100%;
+			margin: auto;
+			background-color: #ecf0f1;
+		}
+		div.contact{
+			width: 80%;
+			margin:auto;
+		}
+		@media screen and (max-width: 320px) {
+		}
+		@media screen and (max-width: 480px) {
+		}
+		@media screen and (max-width: 768px) {
+		}
+		@media screen and (max-width: 1028px) {
+		}
+		</style>
+	</head>
+	<body>
+      	<?php
+        include("_header.php");
+        ?>
+		<div class="">
+			<h1>Contact</h1>
+			<p></p>
+		</div>
+		<?php
+	    include("_footer.php");
+	    ?>
+	</body>
+</html>
